69a18ed0d2ba553d078a4e8c43a07e8431b3f906
Ouyang Xiongyi <hh123okbb@gmail.com>
2015-12-03 01:16:04 +0000

    Fix fonts setup fail

--- fonts/setup.sh
+++ fonts/setup.sh
@@ -5,7 +5,8 @@ set -e
 DIR="$(dirname $0)"
 
 echo "=== Setup fonts..."
-sudo sh -c "mkdir /usr/share/fonts/tmuxfonts"
+sudo apt-get install fontconfig
+sudo sh -c "mkdir -p /usr/share/fonts/tmuxfonts"
 sudo sh -c "cp $DIR/Inconsolata-Powerline.otf /usr/share/fonts/tmuxfonts"
 sudo fc-cache -vf
 
